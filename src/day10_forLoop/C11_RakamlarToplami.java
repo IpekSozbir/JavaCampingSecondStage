d5058612aaa75436e042a722b33155b5276764ac
IpekSozbir <ipekoren@gmail.com>
2025-01-02 14:20:57 +0300

    v15

---
+++ src/day10_forLoop/C11_RakamlarToplami.java
@@ -0,0 +1,25 @@
+package day10_forLoop;
+
+import java.util.Scanner;
+
+public class C11_RakamlarToplami {
+
+    public static void main(String[] args) {
+
+        // Kullanicidan pozitif bir tam sayi alip, rakamlar toplamini yazdirin.
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Lutfen pozitif bir tamsayi giriniz:");
+        int sayi = scan.nextInt();
+        System.out.println("Girmis oldugunuz sayi: " + sayi);
+
+        int basamakSayisi = (sayi+"").length();
+        int rakamlarToplami = 0;
+
+        for (int i = 1; i <=basamakSayisi ; i++) {
+            rakamlarToplami+= sayi%10;
+            sayi /= 10;
+        }
+        System.out.println("Girilen sayinin rakamlar toplami: " + rakamlarToplami);
+    }
+}
